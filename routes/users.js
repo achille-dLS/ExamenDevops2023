4448327180baf306651eb1fd8cb9335ca4046706
achille-ach dls <achille.m.delimburg@student.vinci.be>
2023-09-01 17:22:35 +0200

    ajout test routes et modif package.json

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const User = require('../models/User.js');
 const Utils = require('../utils/utils.js')
 const bcrypt = require('bcrypt');
-const { Utils } = require('hbs');
 const saltRounds = 10;
 
 /* form login / password */
